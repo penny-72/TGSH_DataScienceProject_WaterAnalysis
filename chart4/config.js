14e0969a5b8af3f038210a725c158e632912b90e
Ning Xiang <45084611+penny-72@users.noreply.github.com>
2018-12-29 18:10:23 +0800

    add chart4

---
+++ chart4/config.js
@@ -0,0 +1,250 @@
+var CONFIG = {
+ "data": {
+  "_lastModified": 1541337331504,
+  "hasNameColumn": false,
+  "lastModified": 1541337331504,
+  "nameColumnPosition": 0,
+  "noCache": true,
+  "path": "./data/poster用.xlsx",
+  "reader": "excel",
+  "sheet": "全台各種用水",
+  "timeInColumns": true,
+  "ddfPath": "./data/poster用.xlsx"
+ },
+ "locale": {
+  "filePath": "assets/translation/",
+  "id": "en"
+ },
+ "state": {
+  "entities": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "項目",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "entities_colorlegend": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "項目",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "marker": {
+   "allowSelectMultiple": true,
+   "axis_x": {
+    "allow": {
+     "scales": [
+      "time"
+     ]
+    },
+    "autoconfig": {
+     "index": 0,
+     "type": "time"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "time",
+    "use": "indicator",
+    "which": "time",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "axis_y": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log"
+     ]
+    },
+    "autoconfig": {
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "linear",
+    "use": "indicator",
+    "which": "台灣地區",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "color": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow",
+      "ordinal"
+     ]
+    },
+    "autoconfig": {},
+    "data": "data",
+    "palette": {
+     "_default": "#ffb600"
+    },
+    "paletteLabels": null,
+    "scaleType": "ordinal",
+    "syncModels": [
+     "marker_colorlegend"
+    ],
+    "use": "constant",
+    "which": "_default"
+   },
+   "highlight": [],
+   "label": {
+    "autoconfig": {
+     "includeOnlyIDs": [
+      "name"
+     ],
+     "type": "string"
+    },
+    "data": "data",
+    "scaleType": "ordinal",
+    "use": "property",
+    "which": "項目"
+   },
+   "limit": 5000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities",
+    "time"
+   ],
+   "superHighlight": []
+  },
+  "marker_colorlegend": {
+   "allowSelectMultiple": true,
+   "highlight": [],
+   "hook_geoshape": {
+    "data": "data",
+    "use": "property",
+    "which": "shape_lores_svg"
+   },
+   "hook_rank": {
+    "data": "data",
+    "use": "property",
+    "which": "rank"
+   },
+   "label": {
+    "data": "data",
+    "use": "property",
+    "which": "name"
+   },
+   "limit": 1000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities_colorlegend"
+   ],
+   "superHighlight": []
+  },
+  "time": {
+   "autoconfig": {
+    "type": "time"
+   },
+   "delay": 150,
+   "delayThresholdX2": 90,
+   "delayThresholdX4": 45,
+   "dim": "time",
+   "end": "0105",
+   "endOrigin": null,
+   "endSelected": "0105",
+   "format": {
+    "data": null,
+    "ui": null
+   },
+   "immediatePlay": true,
+   "loop": false,
+   "offset": 0,
+   "playable": true,
+   "playing": false,
+   "record": false,
+   "round": "round",
+   "start": "0095",
+   "startOrigin": null,
+   "startSelected": "0095",
+   "step": 1,
+   "unit": "year",
+   "value": "0105"
+  }
+ },
+ "ui": {
+  "buttons": [
+   "colors",
+   "find",
+   "moreoptions",
+   "fullscreen",
+   "presentation"
+  ],
+  "chart": {
+   "curve": "curveMonotoneX",
+   "labels": {
+    "min_number_of_entities_when_values_hide": 2
+   },
+   "whenHovering": {
+    "hideVerticalNow": false,
+    "higlightValueX": true,
+    "higlightValueY": true,
+    "showProjectionLineX": true,
+    "showProjectionLineY": true,
+    "showTooltip": false
+   }
+  },
+  "datawarning": {
+   "doubtDomain": [],
+   "doubtRange": []
+  },
+  "dialogs": {
+   "dialog": {
+    "find": {
+     "enableSelectShowSwitch": false,
+     "panelMode": "show"
+    }
+   },
+   "moreoptions": [
+    "opacity",
+    "speed",
+    "axes",
+    "colors",
+    "presentation",
+    "about"
+   ],
+   "popup": [
+    "colors",
+    "find",
+    "moreoptions"
+   ],
+   "sidebar": [
+    "colors",
+    "find"
+   ]
+  },
+  "presentation": false,
+  "splash": false
+ },
+ "chartType": "LineChart"
+};
