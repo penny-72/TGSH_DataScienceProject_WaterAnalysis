d57dc70c4a508cd6d084592b2c7e125a87d0cce0
Ning Xiang <45084611+penny-72@users.noreply.github.com>
2019-01-26 22:02:05 +0800

    Add files via upload

---
+++ chart11/config.js
@@ -0,0 +1,218 @@
+var CONFIG = {
+ "data": {
+  "_lastModified": 1548493641605,
+  "hasNameColumn": false,
+  "lastModified": 1548493641605,
+  "nameColumnPosition": 0,
+  "noCache": true,
+  "path": "./data/工業用水各業別歷年分析.xlsx",
+  "reader": "excel",
+  "sheet": "面積與用水量",
+  "timeInColumns": true,
+  "ddfPath": "./data/工業用水各業別歷年分析.xlsx"
+ },
+ "locale": {
+  "filePath": "assets/translation/",
+  "id": "en"
+ },
+ "state": {
+  "entities": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "業別",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "entities_colorlegend": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "業別",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "marker": {
+   "allowSelectMultiple": true,
+   "axis_x": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log"
+     ]
+    },
+    "autoconfig": {
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "linear",
+    "spaceRef": null,
+    "use": "indicator",
+    "which": "用水量(百萬立方公尺)",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "color": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow",
+      "ordinal"
+     ]
+    },
+    "autoconfig": {},
+    "data": "data",
+    "palette": {
+     "_default": "#ffb600"
+    },
+    "paletteLabels": null,
+    "scaleType": "ordinal",
+    "syncModels": [
+     "marker_colorlegend"
+    ],
+    "use": "constant",
+    "which": "_default"
+   },
+   "highlight": [],
+   "label": {
+    "autoconfig": {
+     "includeOnlyIDs": [
+      "name"
+     ],
+     "type": "string"
+    },
+    "data": "data",
+    "scaleType": "ordinal",
+    "use": "property",
+    "which": "業別"
+   },
+   "limit": 1000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities",
+    "time"
+   ],
+   "superHighlight": []
+  },
+  "marker_colorlegend": {
+   "allowSelectMultiple": true,
+   "highlight": [],
+   "hook_geoshape": {
+    "data": "data",
+    "use": "property",
+    "which": "shape_lores_svg"
+   },
+   "hook_rank": {
+    "data": "data",
+    "use": "property",
+    "which": "rank"
+   },
+   "label": {
+    "data": "data",
+    "use": "property",
+    "which": "name"
+   },
+   "limit": 1000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities_colorlegend"
+   ],
+   "superHighlight": []
+  },
+  "time": {
+   "autoconfig": {
+    "type": "time"
+   },
+   "delay": 535,
+   "delayThresholdX2": 90,
+   "delayThresholdX4": 45,
+   "dim": "time",
+   "end": "0105",
+   "endOrigin": null,
+   "endSelected": "0105",
+   "format": {
+    "data": null,
+    "ui": null
+   },
+   "immediatePlay": true,
+   "loop": false,
+   "offset": 0,
+   "playable": true,
+   "playing": false,
+   "record": false,
+   "round": "round",
+   "start": "0087",
+   "startOrigin": null,
+   "startSelected": "0087",
+   "step": 1,
+   "unit": "year",
+   "value": "0105"
+  }
+ },
+ "ui": {
+  "buttons": [
+   "colors",
+   "find",
+   "moreoptions",
+   "fullscreen",
+   "presentation"
+  ],
+  "chart": {},
+  "datawarning": {
+   "doubtDomain": [],
+   "doubtRange": []
+  },
+  "dialogs": {
+   "dialog": {
+    "find": {
+     "enableSelectShowSwitch": false
+    }
+   },
+   "moreoptions": [
+    "opacity",
+    "speed",
+    "colors",
+    "presentation",
+    "about"
+   ],
+   "popup": [
+    "timedisplay",
+    "colors",
+    "find",
+    "moreoptions"
+   ],
+   "sidebar": [
+    "timedisplay",
+    "colors",
+    "find"
+   ]
+  },
+  "presentation": false,
+  "splash": false
+ },
+ "chartType": "BarRankChart"
+};
