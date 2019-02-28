9f527cac91925409e983f53f771ce6cb0aafb244
WanChen0104 <45084695+WanChen0104@users.noreply.github.com>
2019-02-28 23:36:42 +0800

    Add files via upload
    重˙新上船水庫的表格

---
+++ chart16/config.js
@@ -0,0 +1,275 @@
+var CONFIG = {
+ "data": {
+  "_lastModified": 1551156026567,
+  "hasNameColumn": false,
+  "lastModified": 1551156026567,
+  "nameColumnPosition": 0,
+  "noCache": true,
+  "path": "./data/水庫 (2).xlsx",
+  "reader": "excel",
+  "sheet": "整理好的水庫",
+  "timeInColumns": false,
+  "ddfPath": "./data/水庫 (2).xlsx"
+ },
+ "locale": {
+  "filePath": "assets/translation/",
+  "id": "en"
+ },
+ "state": {
+  "entities": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "水庫名稱",
+   "filter": {},
+   "show": {
+    "水庫名稱": {
+     "$in": [
+      "德基水庫",
+      "日月潭水庫",
+      "曾文水庫",
+      "牡丹水庫",
+      "石門水庫",
+      "翡翠水庫"
+     ]
+    }
+   },
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "entities_colorlegend": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "地區分色",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "marker": {
+   "allowSelectMultiple": true,
+   "axis_x": {
+    "allow": {
+     "scales": [
+      "time"
+     ]
+    },
+    "autoconfig": {
+     "index": 0,
+     "type": "time"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "time",
+    "use": "indicator",
+    "which": "日期",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "axis_y": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log"
+     ]
+    },
+    "autoconfig": {
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "linear",
+    "spaceRef": null,
+    "use": "indicator",
+    "which": "蓄水百分比",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "color": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow",
+      "ordinal"
+     ]
+    },
+    "autoconfig": {},
+    "data": "data",
+    "palette": {
+     "0": "#4cd843",
+     "1": "#e83739",
+     "2": "#ff7f00",
+     "3": "#c027d4",
+     "4": "#d66425",
+     "5": "#0ab8d8",
+     "6": "#bcfa83",
+     "7": "#ff8684",
+     "8": "#ffb04b",
+     "9": "#f599f5",
+     "10": "#f4f459",
+     "11": "#7fb5ed",
+     "_default": "#ffb600"
+    },
+    "paletteLabels": null,
+    "scaleType": "ordinal",
+    "spaceRef": null,
+    "syncModels": [
+     "marker_colorlegend"
+    ],
+    "use": "indicator",
+    "which": "地區分色"
+   },
+   "highlight": [],
+   "label": {
+    "autoconfig": {
+     "includeOnlyIDs": [
+      "name"
+     ],
+     "type": "string"
+    },
+    "data": "data",
+    "scaleType": "ordinal",
+    "use": "property",
+    "which": "水庫名稱"
+   },
+   "limit": 5000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities",
+    "time"
+   ],
+   "superHighlight": []
+  },
+  "marker_colorlegend": {
+   "allowSelectMultiple": true,
+   "highlight": [],
+   "hook_geoshape": {
+    "data": "data",
+    "use": "property",
+    "which": "shape_lores_svg"
+   },
+   "hook_rank": {
+    "data": "data",
+    "use": "property",
+    "which": "rank"
+   },
+   "label": {
+    "data": "data",
+    "use": "property",
+    "which": "name"
+   },
+   "limit": 1000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities_colorlegend"
+   ],
+   "superHighlight": []
+  },
+  "time": {
+   "autoconfig": {
+    "type": "time"
+   },
+   "delay": 150,
+   "delayThresholdX2": 90,
+   "delayThresholdX4": 45,
+   "dim": "日期",
+   "end": "2019-02",
+   "endOrigin": null,
+   "endSelected": "2019-02",
+   "format": {
+    "data": null,
+    "ui": null
+   },
+   "immediatePlay": true,
+   "loop": false,
+   "offset": 0,
+   "playable": true,
+   "playing": false,
+   "record": false,
+   "round": "round",
+   "start": "2003-02",
+   "startOrigin": null,
+   "startSelected": "2003-02",
+   "step": 1,
+   "unit": "month",
+   "value": "2019-02"
+  }
+ },
+ "ui": {
+  "buttons": [
+   "colors",
+   "find",
+   "moreoptions",
+   "fullscreen",
+   "presentation"
+  ],
+  "chart": {
+   "curve": "curveMonotoneX",
+   "labels": {
+    "min_number_of_entities_when_values_hide": 2
+   },
+   "whenHovering": {
+    "hideVerticalNow": false,
+    "higlightValueX": true,
+    "higlightValueY": true,
+    "showProjectionLineX": true,
+    "showProjectionLineY": true,
+    "showTooltip": false
+   }
+  },
+  "datawarning": {
+   "doubtDomain": [],
+   "doubtRange": []
+  },
+  "dialogs": {
+   "dialog": {
+    "find": {
+     "enableSelectShowSwitch": false,
+     "panelMode": "show"
+    }
+   },
+   "moreoptions": [
+    "opacity",
+    "speed",
+    "axes",
+    "colors",
+    "presentation",
+    "about"
+   ],
+   "popup": [
+    "colors",
+    "find",
+    "moreoptions"
+   ],
+   "sidebar": [
+    "colors",
+    "find"
+   ]
+  },
+  "presentation": false,
+  "splash": false
+ },
+ "chartType": "LineChart"
+};
